f99f8749ab267a9508f174b36a0dede1967ec561
halpma <103608480+halpma@users.noreply.github.com>
2022-09-01 10:03:32 +1200

    Add files via upload

---
+++ Lesson 1/input_from_user.py
@@ -0,0 +1,24 @@
+# O W Holmes
+# User imput
+
+# num = int(input("Enter a number: "))
+# add = num + 1
+
+# output
+# print(add)
+
+
+# num = float(input("Enter a number: "))
+# add = num + 1
+
+# output
+# print(add)
+
+
+# li = list(input("Enter a number of 5 digits: "))
+# print(li)
+
+# Taking input from user as tuple 
+
+num = tuple(input ("Enter a number"))
+print(num)
